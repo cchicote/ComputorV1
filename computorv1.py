47a5d1e64c43737d7d50c6761a36e2d505b57cdf
cchicote <cchicote@student.42.fr>
2020-07-25 20:16:18 +0200

    almost done, need some debugging

--- computorv1.py
+++ computorv1.py
@@ -40,16 +40,19 @@ def parse_math_expr(expr):
     sign = '+'
     cells = []
     cell = []
-
+    
+    print("Expr: [%s]" % (expr))
     if expr[len(expr) - 2] in signs:
         print("WRONG FORMAT, CAN'T FINISH BY A SIGN")
         exit(1)
 
     for char in expr:
         
+        print(char)
         # DIGIT
         if char.isdigit():
             if isPow is True and 'X' not in cell:
+                print("CELL: [%s] [%s]" % (char, cell))
                 print("CAN'T MULTIPLY NUMBERS")
                 exit(1)
                 #cell.append('*')
@@ -146,6 +149,7 @@ def parse_math_expr(expr):
             sign = '+'
             equalIsPassed = True
             isNum = False
+            isPow = False
 
     if len(cell) > 0:
         cell = ''.join(cell)
@@ -158,6 +162,8 @@ def eval_math_expr(cells):
     sorted_cells['0'] = []
     sorted_cells['1'] = []
     sorted_cells['2'] = []
+    reduced_cells = {}
+    first_cell = True
     for cell in cells:
         if "X^1" in cell:
             sorted_cells['1'].append(cell.split('X')[0])
@@ -168,15 +174,72 @@ def eval_math_expr(cells):
     print(sorted_cells)
     for degree, sorted_cell in sorted_cells.items():
         try:
-            sorted_cell = eval(''.join(sorted_cell))
-            sorted_cells[degree] = sorted_cell
+            reduced_cells[degree] = eval(''.join(sorted_cell))
         except SyntaxError:
+            # Throws a syntax error if eval an empty string
+            reduced_cells[degree] = 0
             pass
     print("Reduced form: ", end='')
-    for degree, sorted_cell in sorted_cells.items():
-        if sorted_cell:
-            print("%s * X^%s" % (sorted_cell, degree), end=' ')
+    for degree, reduced_cell in reduced_cells.items():
+        if reduced_cell:
+            if first_cell is False:
+                if reduced_cell > 0:
+                    print("+ %s * X^%s" % (reduced_cell, degree), end=' ')
+                elif reduced_cell < 0:
+                    print("- %s * X^%s" % (reduced_cell * -1, degree), end=' ')
+            else:
+                print("%s * X^%s" % (reduced_cell, degree), end=' ')
+                first_cell = False
     print("= 0")
+    return reduced_cells
+
+def resolve_deg_two(cells):
+    a = cells['2']
+    b = cells['1']
+    c = cells['0']
+    if a == 0:
+        print("GROS PROBLEME, ON NE DEVRAIT PAS LANCER DEG_TWO SI A = 0")
+    delta = b * b - 4 * a * c
+    if delta > 0:
+        print("Discriminant is strictly positive, the two solutions are:")
+        x2 = (-b + delta ** 0.5) / (2 * a)
+        x1 = (-b - delta ** 0.5) / (2 * a)
+        if x1.is_integer() is False:
+            print("{0:.6f}".format(x1))
+        else:
+            print(x1)
+        if x2.is_integer() is False:
+            print("{0:.6f}".format(x2))
+        else:
+            print(x2)
+    elif delta == 0:
+        print("Discriminant is equal to zero, the solution is:")
+        x = (-b) / (2 * a)
+        if x.is_integer() is False:
+            print("{0:.6f}".format(x))
+        else:
+            print(x)
+    else:
+        print("Discriminant is strictly negative, there is no solution.")
+
+def resolve_deg_one(cells):
+    a = 1
+    b = 0
+    for degree, cell in cells.items():
+        if degree == '1':
+            a = cell
+        elif degree == '0':
+            b = cell
+    x = -b / a
+    print("The solution is: %s" % (x))
+
+def print_degree(cells):
+    if cells['2'] != 0:
+        print("Polynomial degree: 2")
+    elif cells['1'] != 0:
+        print("Polynomial degree: 1")
+    elif cells['0'] != 0:
+        print("Polynomial degree: 0")
 
 def main():
     try:
@@ -184,7 +247,13 @@ def main():
             math_expr = input('Your mathematical expression: ')
             expr = setup_and_apply_rules(math_expr)
             cells = parse_math_expr(expr)
-            eval_math_expr(cells)
+            sorted_cells = eval_math_expr(cells)
+            print_degree(sorted_cells)
+            if sorted_cells['2'] != 0:
+                resolve_deg_two(sorted_cells)
+            else:
+                resolve_deg_one(sorted_cells)
+                
     except KeyboardInterrupt:
         pass
 
